c0f46c3da34af78f2f7a27fefdfcd23dadba666f
thynash <24mc3033@rgipt.ac.in>
2025-06-14 12:11:36 +0000

    this is the changed version

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import React from "react";
 import { HeroSection } from "./components/Herosection";
-import { Footer } from "./components/Footer";
+import { AboutSection } from "./components/AboutSection";
+import { ExperienceEducation } from "./components/ExperienceEducation";
+import { ProjectsSection } from "./components/ProjectsSection";
+import { SkillsSection } from "./components/SkillsSection";
+import { AchievementsSection } from "./components/AchievementsSection";
+import { FooterSection } from "./components/FooterSection";
+
 function App() {
   return (
-    <>
+    <main className="bg-black text-white scroll-smooth">
       <HeroSection />
-      <Footer />
-    </>
+      <AboutSection />
+      <ExperienceEducation />
+      <ProjectsSection />
+      <SkillsSection />
+      <AchievementsSection />
+      <FooterSection />
+    </main>
   );
 }
+
+export default App;
 
